40f4784dac4755d8ef86af4f8ab2aae023afee02
malxdeutsch <malxdeutsch@gmail.com>
2021-10-28 16:10:18 +0300

    f

--- Week4/Day5/dailychallenge.py
+++ Week4/Day5/dailychallenge.py
@@ -1,5 +1,5 @@
 new_input = input("List some words seperated by commas")
 
-new_list = [word for word in sorted(new_input.split())]
+new_list = [word+word.join(",") for word in sorted(new_input.split(","))]
 
 print(new_list) 
