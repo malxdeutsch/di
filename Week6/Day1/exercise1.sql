c022806b353d571cf9b4333d35de7de6b0ca74f7
malxdeutsch <malxdeutsch@gmail.com>
2021-11-07 12:18:19 +0200

    f

---
+++ Week6/Day1/exercise1.sql
@@ -0,0 +1,12 @@
+-- Database: public
+
+-- DROP DATABASE public;
+
+--select * from items;
+--select * from items where price > 80
+--select * from items where price <= 300
+--select * from customers where l_name = 'Smith'
+--select * from customers where l_name = 'Jones'
+--select * from customers where f_name != 'Scott'
+
+
