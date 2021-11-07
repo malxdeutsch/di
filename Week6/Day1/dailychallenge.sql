f09716f04d5ca3a13167e5c6df72b54d882a626e
malxdeutsch <malxdeutsch@gmail.com>
2021-11-07 14:28:31 +0200

    f

---
+++ Week6/Day1/dailychallenge.sql
@@ -0,0 +1,8 @@
+-- Database: Hollywood
+
+-- DROP DATABASE "Hollywood";
+
+-- select count(*) from actors
+
+INSERT INTO actors (first_name, last_name, age, number_oscars)
+VALUES ('Molly','','11/02/1969')
