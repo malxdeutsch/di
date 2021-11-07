47ba0633f638ec63b5404abf1acaa2098d70a0b6
malxdeutsch <malxdeutsch@gmail.com>
2021-11-07 14:11:40 +0200

    f

---
+++ Week6/Day1/exercise2.sql
@@ -0,0 +1,13 @@
+-- Database: bootcamp
+
+-- DROP DATABASE bootcamp;
+
+-- select * from students where student_id = 2;
+-- select * from students where f_name = 'Marc' and l_name= 'Benichou'
+-- select * from students where f_name = 'Marc' or l_name= 'Benichou'
+-- select * from students where f_name like '%a%';
+-- select * from students where f_name like 'A%'
+-- select * from students where f_name like '%a'
+-- select * from students where f_name like '%a-';
+-- select * from students where student_id = 1 and student_id = 3
+--select * from students where birth_date >= '2000/01/01';
